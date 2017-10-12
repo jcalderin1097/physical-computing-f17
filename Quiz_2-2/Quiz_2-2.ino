8a263fed0c3e119c97e980134125d7f79eab641a
Jose Calderin <jose.calderin@mail.citytech.cuny.edu>
2017-10-12 17:48:34 -0400

    Quiz 2

---
+++ Quiz_2-2/Quiz_2-2.ino
@@ -0,0 +1,20 @@
+int buttonPin = 5;
+int ledPin= 4;
+void setup() {
+  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode (ledPin,OUTPUT);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  int buttonState = digitalRead(buttonPin);
+  Serial.println(buttonState);
+
+  if (buttonState == 0){
+ 
+  digitalWrite(ledPin,HIGH);
+  delay(500);
+  digitalWrite(ledPin,LOW); 
+  delay(500);}
+}
